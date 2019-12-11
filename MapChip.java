16783bd0b5035a485339c2728b9b0579b9e00d56
Fn <fn.nk.2002@gmail.com>
2019-12-11 21:41:52 +0900

    マップチップの描画処理

--- MapChip.java
+++ MapChip.java
@@ -2,15 +2,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 class MapChip extends ImageView {
-    private byte fieldNumber;
-    MapChip(){
+    private int fieldNumber;
+    MapChip() {
         super();
     }
-    MapChip(String url, byte fieldNumber){
+    MapChip(Image image) {
+        this(image, 0);
+    }
+    MapChip(String url) {
+        this(new Image(url), 0);
+    }
+    MapChip(String url, int fieldNumber) {
         this(new Image(url), fieldNumber);
     }
-    MapChip(Image image, byte fieldNumber){
+    MapChip(Image image, int fieldNumber) {
         super(image);
+        this.fieldNumber = fieldNumber;
+    }
+    int getFieldNumber() {
+        return this.fieldNumber;
+    }
+    void setFieldNumber(int fieldNumber) {
         this.fieldNumber = fieldNumber;
     }
 } 
---
+++ MapEditor.java
@@ -0,0 +1,132 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+
+/**
+ * MapEditorクラス
+ * ・レイアウトの配置、管理
+ * ・各メニューのメソッドの管理
+ * ・ファイルのオープン、保存
+ */
+
+public class MapEditor extends Application {
+    /* 現在の状態(描画モードが消しゴムモード)を表す列挙型 */
+    private enum MouseFlag {
+        draw,
+        erase
+    };
+    /* 初期設定では描画モード */
+    private MouseFlag mFlag = MouseFlag.draw;
+    /* マップフィールドの幅と高さの最大値 */
+    private final int row = 46;
+    private final int column = 29;
+    /* 配置するマップチップの二次元配列 */
+    private MapChip[][] mapField = new MapChip[row][column];
+    /* パレット(マップチップ)の情報 */
+    private Image[] mapChips = new Image[9];
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("マップエディタ");
+        stage.setWidth(1880);
+        stage.setHeight(1000);
+
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("ファイル");
+        Menu editMenu = new Menu("編集");
+        MenuItem makeNewFile = new MenuItem("新規作成");
+        MenuItem openFile = new MenuItem("開く");
+        MenuItem saveFile = new MenuItem("保存");
+        MenuItem endEdit = new MenuItem("終了");
+        MenuItem eraser = new MenuItem("消しゴム");
+        MenuItem allDelete = new MenuItem("全消去");
+        makeNewFile.setOnAction(event -> newFile());
+        openFile.setOnAction(event -> openFile());
+        saveFile.setOnAction(event -> saveFile());
+        endEdit.setOnAction(event -> endEdit());
+        eraser.setOnAction(event -> modeChange());
+        allDelete.setOnAction(event -> fieldDelete());
+        fileMenu.getItems().addAll(makeNewFile, openFile, saveFile, endEdit);
+        editMenu.getItems().addAll(allDelete, eraser);
+        menuBar.getMenus().addAll(fileMenu, editMenu);
+
+        GridPane gridPane = new GridPane();
+        initMapChips();
+        initMapField(gridPane);
+
+        VBox root = new VBox();
+        root.getChildren().addAll(menuBar, gridPane);
+        Scene scene = new Scene(root);
+        scene.setOnMouseDragged(event -> mouseOnAction(event));
+        stage.setScene(scene);
+        stage.show();
+    }
+    private void initMapChips() {
+        mapChips[0] = new Image("mapChip\\0.bmp");
+        mapChips[1] = new Image("mapChip\\1.bmp");
+        mapChips[2] = new Image("mapChip\\1.bmp");
+        mapChips[3] = new Image("mapChip\\1.bmp");
+        mapChips[4] = new Image("mapChip\\1.bmp");
+        mapChips[5] = new Image("mapChip\\1.bmp");
+        mapChips[6] = new Image("mapChip\\1.bmp");
+        mapChips[7] = new Image("mapChip\\1.bmp");
+        mapChips[8] = new Image("mapChip\\1.bmp");
+    }
+    private void initMapField(GridPane gridPane) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                mapField[i][j] = new MapChip(mapChips[0], 0);
+                GridPane.setConstraints(mapField[i][j], i, j);
+                gridPane.getChildren().addAll(mapField[i][j]);
+            }
+        }
+    }
+    private void mouseOnAction(MouseEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (mFlag) {
+        case draw:
+            draw(x, y);
+            break;
+        case erase:
+            delete(x, y);
+            break;
+        }
+    }
+    private void draw(int x, int y) {
+        // MenuBarの長さ分yを調整
+        x /= 32;
+        y = (y - 20) / 32;
+        try {
+            mapField[x][y].setImage(mapChips[1]);
+            mapField[x][y].setFieldNumber(1);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return;
+        }
+    }
+    private void delete(int x, int y) {
+        x /= 32;
+        y = (y - 20) / 32;
+        try {
+            mapField[x][y].setImage(mapChips[0]);
+            mapField[x][y].setFieldNumber(0);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return;
+        }
+    }
+    private void newFile(){}
+    private void openFile(){}
+    private void saveFile(){}
+    private void endEdit(){}
+    private void modeChange(){}
+    private void fieldDelete(){}
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
